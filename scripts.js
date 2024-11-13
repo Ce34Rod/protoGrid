48551c3c9b26ae4bbcc32e7720a962fbea7e1220
Ce34Rod <139182790+Ce34Rod@users.noreply.github.com>
2024-11-12 20:25:23 -0600

    Header Anchors

--- scripts.js
+++ scripts.js
@@ -1,17 +1,7 @@
 // Global Variables
 
-const menuLink = document.querySelector(".menu a");
-const header = document.querySelector("header");
-
 // Listeners
-
-document.querySelector(".menu").addEventListener("click", toggleMenu);
-
 
 
 // Functions
 
-function toggleMenu() {
-    menuLink.classList.toggle("active");
-    header.classList.toggle("black");
-}
