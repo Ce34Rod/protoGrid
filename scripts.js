bb9df468396bd9c2e828fabba9758e84d64b08c9
Ce34Rod <139182790+Ce34Rod@users.noreply.github.com>
2024-11-13 00:13:11 -0600

    First Complete Version

--- scripts.js
+++ scripts.js
@@ -1,7 +1,12 @@
-// Global Variables
+// Slide Show
+const slideshowImages = document.querySelectorAll(".slideshow-img");
+const nextImageDelay = 4000;
+let currentImageCounter = 0;
+setInterval(nextImage, nextImageDelay);
+slideshowImages[currentImageCounter].style.opacity = 1;
 
-// Listeners
-
-
-// Functions
-
+function nextImage() {
+    slideshowImages[currentImageCounter].style.opacity = 0;
+    currentImageCounter = (currentImageCounter + 1) % slideshowImages.length;
+    slideshowImages[currentImageCounter].style.opacity = 1;
+}
