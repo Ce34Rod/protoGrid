54ef6c764cea4cfc17f7306143ae7cfacb5d6801
Ce34Rod <139182790+Ce34Rod@users.noreply.github.com>
2024-11-25 21:55:08 -0600

    Works Pricing

---
+++ scripts-pricing.js
@@ -0,0 +1,24 @@
+function loadHeader () {
+    const header = document.getElementById("header");
+    const isMobile  = window.matchMedia("(max-width: 1000px) and (min-resolution: 3dppx)").matches;
+
+    if(isMobile) {
+        header.innerHTML = `<a href="index.html" class="logo-box"><img src="images/CERNlogo.png" alt="logo" class="logo-img-small" /></a>
+        <div class="dropdown">
+            <button onclick="myFunction()" class="dropbtn">| | |</button>
+            <div id="myDropdown" class="dropdown-content">
+              <a href="about.html">About</a>
+              <a href="pricing.html">Pricing</a>
+              <a href="more-photos.html">More Photos</a>
+            </div>`
+    }
+    else {
+        header.innerHTML = `<a class="small-anchor-home" href="index.html">Home</a>
+        <a class="small-anchor-about" href="about.html">About</a>
+        <a href="index.html" class="logo-box"><img src="CERNlogo.svg" alt="logo" class="logo-img" /></a>
+        <a class="small-anchor-pricing" href="pricing.html">Pricing</a>
+        <a class="small-anchor-more-photos" href="more-photos.html">More Photos</a>`
+    }
+}
+window.addEventListener("load", loadHeader);
+window.addEventListener("resize", loadHeader);
