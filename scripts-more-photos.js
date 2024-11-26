9eee7c90ff51336ba022921502d16a6a41fa5824
Ce34Rod <139182790+Ce34Rod@users.noreply.github.com>
2024-11-26 00:10:30 -0600

    Beta

--- scripts-more-photos.js
+++ scripts-more-photos.js
@@ -1,5 +1,43 @@
-const flipPhoto = document.getElementById('flipPhoto');
+function loadHeader() {
+  const header = document.getElementById("header");
+  const isMobile = window.matchMedia(
+    "(max-width: 1000px) and (min-resolution: 3dppx)"
+  ).matches;
 
-flipPhoto.addEventListener('click', () => {
-  flipPhoto.classList.toggle('flipped');
-});
+  if (isMobile) {
+    header.innerHTML = `<a href="index.html" class="logo-box"><img src="images/CERNlogo.png" alt="logo" class="logo-img-small" /></a>
+      <div class="dropdown">
+          <button onclick="myFunction()" class="dropbtn">| | |</button>
+          <div id="myDropdown" class="dropdown-content">
+            <a href="about.html">About</a>
+            <a href="pricing.html">Pricing</a>
+            <a href="more-photos.html">More Photos</a>
+          </div>`;
+  } else {
+    header.innerHTML = `<a class="small-anchor-home" href="index.html">Home</a>
+      <a class="small-anchor-about" href="about.html">About</a>
+      <a href="index.html" class="logo-box"><img src="CERNlogo.svg" alt="logo" class="logo-img" /></a>
+      <a class="small-anchor-pricing" href="pricing.html">Pricing</a>
+      <a class="small-anchor-more-photos" href="more-photos.html">More Photos</a>`;
+  }
+}
+window.addEventListener("load", loadHeader);
+window.addEventListener("resize", loadHeader);
+
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function (event) {
+  if (!event.target.matches(".dropbtn")) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+      }
+    }
+  }
+};
--- scripts-pricing.js
+++ scripts-pricing.js
@@ -22,3 +22,23 @@ function loadHeader () {
 }
 window.addEventListener("load", loadHeader);
 window.addEventListener("resize", loadHeader);
+
+
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
