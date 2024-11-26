f63fd8f06a37f845e8f6bbce2c622a527a86684f
Ce34Rod <139182790+Ce34Rod@users.noreply.github.com>
2024-11-25 20:53:22 -0600

    Mobile Index Complete

--- scripts.js
+++ scripts.js
@@ -1,19 +1,27 @@
 function loadHeader () {
     const header = document.getElementById("header");
-    const isMobile  = window.matchMedia("(max-width: 1000px").matches;
+    const isMobile  = window.matchMedia("(max-width: 1000px)").matches;
 
     if(isMobile) {
-        header.innerHTML = `<a href="index.html"><img src="CERNlogo.svg" alt="logo" class="logo-img" /></a>
+        header.innerHTML = `<a href="index.html" class="logo-box"><img src="images/CERNlogo.png" alt="logo" class="logo-img-small" /></a>
         <div class="dropdown">
-            <button onclick="myFunction()" class="dropbtn">Dropdown</button>
+            <button onclick="myFunction()" class="dropbtn">| | |</button>
             <div id="myDropdown" class="dropdown-content">
-              <a href="#home">Home</a>
-              <a href="#about">About</a>
-              <a href="#contact">Contact</a>
+              <a href="about.html">About</a>
+              <a href="pricing.html">Pricing</a>
+              <a href="more-photos.html">More Photos</a>
             </div>`
+    }
+    else {
+        header.innerHTML = `<a class="small-anchor-home" href="index.html">Home</a>
+        <a class="small-anchor-about" href="about.html">About</a>
+        <a href="index.html" class="logo-box"><img src="CERNlogo.svg" alt="logo" class="logo-img" /></a>
+        <a class="small-anchor-pricing" href="pricing.html">Pricing</a>
+        <a class="small-anchor-more-photos" href="more-photos.html">More Photos</a>`
     }
 }
 window.addEventListener("load", loadHeader);
+window.addEventListener("resize", loadHeader);
 
 
 
